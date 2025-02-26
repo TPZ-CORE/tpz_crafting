7d281aecc8c6f88e5be60ff56a495bf10d32eb19
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-26 13:07:49 +0200

    Delete tpz_crafting directory

--- tpz_crafting/[EXTRA]/crafting.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `crafting` (
-  `job` varchar(45) NOT NULL,
-  `unlocked_recipes` longtext DEFAULT '[]',
-  `level` int(11) DEFAULT 1,
-  `experience` int(11) DEFAULT 0,
-  `actions` int(11) DEFAULT 0,
-  PRIMARY KEY (`job`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
--- tpz_crafting/html/js/locales/locales-en.js
+++
@@ -1,6 +0,0 @@
-var Locales = new Object();
-
-Locales.IngredientsTitle      = " (X%s Product-s) Ingredients"; // -- %s returns the given quantity of the crafting recipe.
-Locales.InsufficientKnowledge = "INSUFFICIENT KNOWLEDGE";
-Locales.RequiredBlueprint     = "%s is required to acquire the knowledge of the recipe.\nIf you have the blueprint into your belongings, would you like to read to acquire this knowledge?";
-Locales.ReadBlueprintActionButton = "Read Blueprint";
