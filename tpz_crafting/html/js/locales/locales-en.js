dcd19eb8ce037f18c8b8498ff00263d32d325f2c
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-11-26 11:53:31 +0200

    Update locales-en.js

--- tpz_crafting/html/js/locales/locales-en.js
+++ tpz_crafting/html/js/locales/locales-en.js
@@ -4,3 +4,5 @@ Locales.IngredientsTitle      = " (X%s Product-s) Ingredients"; // -- %s returns
 Locales.InsufficientKnowledge = "INSUFFICIENT KNOWLEDGE";
 Locales.RequiredBlueprint     = "%s is required to acquire the knowledge of the recipe.\nIf you have the blueprint into your belongings, would you like to read to acquire this knowledge?";
 Locales.ReadBlueprintActionButton = "Read Blueprint";
+Locales.RepairDescription = 'Tool repairs involve fixing or restoring hand or power tools so they work properly again, including replacing worn parts, adjusting components, and ensuring safe, reliable operation.';
+Locales.RepairButtonAccept = 'REPAIR NOW';
